14271724224c210599eb0be0983571712c550988
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-06-06 21:35:44 +0200

    feat: rerender

--- main.go
+++ main.go
@@ -2,20 +2,55 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 type model struct {
 	problems []string
+	sub 	chan struct{}
+	responses int
+}
+
+// A message used to indicate that activity has occurred. In the real world (for
+// example, chat) this would contain actual data.
+type responseMsg struct{}
+
+// Simulate a process that sends events at an irregular interval in real time.
+// In this case, we'll send events on the channel at a random interval between
+// 100 to 1000 milliseconds. As a command, Bubble Tea will run this
+// asynchronously.
+func listenForActivity(sub chan struct{}) tea.Cmd {
+	return func() tea.Msg {
+		for {
+			time.Sleep(time.Millisecond * time.Duration(rand.Int63n(900)+100))
+			sub <- struct{}{}
+		}
+	}
+}
+
+// A command that waits for the activity on a channel.
+func waitForActivity(sub chan struct{}) tea.Cmd {
+	return func() tea.Msg {
+		return responseMsg(<-sub)
+	}
 }
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return tea.Batch(
+		listenForActivity(m.sub), // generate activity
+		waitForActivity(m.sub),   // wait for activity
+	)
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case responseMsg:
+		m.responses++                    // record external activity
+		return m, waitForActivity(m.sub) // wait for next event
+
 	case tea.KeyMsg:
 		switch msg.String() {
 
@@ -28,7 +63,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	s := ""
+	s := fmt.Sprintf("%d responses\n\n", m.responses)
 
 	for _, problem := range m.problems {
 		s += fmt.Sprintf("%s\n", problem)
@@ -40,6 +75,7 @@ func (m model) View() string {
 func main() {
 	p := tea.NewProgram(model{
 		problems: []string{"Workplace Issues", "Emptiness", "Friendship Issues"},
+		sub:     make(chan struct{}),
 	})
 
 	if err := p.Start(); err != nil {
